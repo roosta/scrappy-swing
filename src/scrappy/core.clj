38dba9361d5f4f2bd50ca9b67cc800ead6345cbf
Daniel Berg <mail@roosta.sh>
2018-03-12 22:46:47 +0100

    Add north layout

--- src/scrappy/core.clj
+++ src/scrappy/core.clj
@@ -3,6 +3,12 @@
             [seesaw.font :as f])
   (:gen-class))
 
+(def config (atom {:font-size 24
+                   :root "/home/roosta/netmedia/files"}))
+
+#_(config! lbl :font (font :name :monospaced
+                         :style #{:bold :italic}
+                         :size 18))
 
 (def frame (s/frame :title "Scrappy"))
 
@@ -15,11 +21,26 @@
 (def area (s/text :multi-line? true :font "MONOSPACED-PLAIN-14"
                   :text (java.net.URL. "http://clojure.com")))
 
-
 (def split (s/left-right-split
             (s/scrollable lb)
             (s/scrollable area)
             :divider-location 1/3))
+
+(def group (s/button-group))
+
+(def north (s/border-panel
+            :west (s/horizontal-panel :items ["Root folder: "
+                                              (s/text :text (:root @config))]
+                                )
+            :east (s/grid-panel :items ["Font size:"
+                                        (s/text :text (:font-size @config)
+                                                :editable? false)
+                                        (s/button :text "-" :group group)
+                                        (s/button :text "+" :group group)]
+                                :columns 4
+                                :hgap 5)))
+
+(def root-field [(s/text "This is a text field.")])
 
 (def rbs (for [i [:source :doc]]
            (s/radio :id i :class :type :text (name i))))
@@ -29,7 +50,7 @@
   (s/native!)
   (-> frame s/pack! s/show!)
   (display (s/border-panel
-            :north (s/horizontal-panel :items rbs)
+            :north north
             :center split
             :vgap 5 :hgap 5 :border 5))
 

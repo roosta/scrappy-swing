23791deae73e35b1035423b568f7603191d29fd3
Daniel Berg <mail@roosta.sh>
2018-03-13 20:46:55 +0100

    Add reload

--- src/scrappy/core.clj
+++ src/scrappy/core.clj
@@ -7,58 +7,63 @@
                    :root "/home/roosta/netmedia/files"}))
 
 #_(config! lbl :font (font :name :monospaced
-                         :style #{:bold :italic}
-                         :size 18))
+                           :style #{:bold :italic}
+                           :size 18))
 
-;; (defn display [content]
-;;   (s/config! frame :content content)
-;;   content)
-
-(defn make-ui
+(defn content
   []
-  (s/frame
-   :title "Scrappy"
-   :on-close :exit
-   :content (s/border-panel
-             :north (s/border-panel
-                     :west (s/horizontal-panel
-                            :items ["Root folder: "
-                                    (s/text
-                                     :text (:root @config)
-                                     :columns 60)])
-                     :east (s/horizontal-panel
-                            :items ["Font size: "
-                                    (s/text
-                                     :text (:font-size @config)
-                                     :editable? false)
-                                    (s/button
-                                     :text "-"
-                                     :id "font-minus")
-                                    (s/button
-                                     :text "+"
-                                     :id "font-plus")]))
-             :center (s/left-right-split
-                      (s/scrollable
-                       (s/listbox
-                        :model (-> 'seesaw.core ns-publics keys sort)))
-                      (s/scrollable
-                       (s/text
-                        :multi-line? true
-                        :font "MONOSPACED-PLAIN-14"
-                        :text (java.net.URL. "http://clojure.com")))
-                      :divider-location 1/3)
-             :vgap 5 :hgap 5 :border 5)))
+  (s/border-panel
+   :north (s/border-panel
+           :west (s/horizontal-panel
+                  :items ["Root folder: "
+                          (s/text
+                           :text (:root @config)
+                           :columns 60)])
+           :east (s/horizontal-panel
+                  :items ["Font size: "
+                          (s/text
+                           :text (:font-size @config)
+                           :editable? false)
+                          (s/button
+                           :text "-"
+                           :id "font-minus")
+                          (s/button
+                           :text "+"
+                           :id "font-plus")]))
+   :center (s/left-right-split
+            (s/scrollable
+             (s/listbox
+              :model (-> 'seesaw.core ns-publics keys sort)))
+            (s/scrollable
+             (s/text
+              :multi-line? true
+              :font "MONOSPACED-PLAIN-14"
+              :text (java.net.URL. "http://clojure.com")))
+            :divider-location 1/3)
+   :vgap 5 :hgap 5 :border 5)
+  )
+
+(def frame (s/frame :title "Scrappy"
+                    ;; :on-close :exit
+                    :content content))
+
+(defn reload
+  []
+  (s/config! frame :content (content)))
+
+(reload)
 
 (defn run
   []
   (s/invoke-later
-   (-> (make-ui)
+   (-> frame
        s/pack!
        s/show!)))
 
 (defn -main [& args]
   (s/native!)
   (run)
+
   ;; (s/config! frame :font (f/font :size (:font-size @config)))
 
   #_(let [b (s/select frame [:#font-minus])]
